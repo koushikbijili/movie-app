20a5a957fa9b3c496844f7db1cbabd6840674806
koushikbijili <koushikbijili48@gmail.com>
2025-11-03 17:50:05 +0530

    Added backend folder with server.js and package.json

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+// server.js
+const express = require("express");
+const app = express();
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Backend is working! 🎬");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
